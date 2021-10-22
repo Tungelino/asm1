6c7db3499683fa06d2be5bd29314e0bd7eeba6c5
TungNguyen <tungnguyen.neu@gmail.com>
2021-10-22 11:41:02 +0900

    asm1-commit1-xu ly ham onClick

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap'
+import StaffList from './components/StaffListComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color='primary'>
+        <div className='container'>
+          <NavbarBrand>Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+        </div>
+      </Navbar>
+      <StaffList />
+      {/* <div >Bấm vào tên để xem thông tin</div> */}
+
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { DEPARTMENTS, ROLE, STAFFS } from '../shared/staffs'
+import dateFormat from "dateformat"
+import { useState } from "react";
+import './staff.css'
+
+function RenderStaff({ data, onClick }) {
+    return (
+        <div className='col-lg-4 col-md-6'>
+            <div className='staff-name'>
+                <p onClick={() => onClick(data)}>{data.name}</p>
+            </div>
+        </div>
+    )
+}
+
+function StaffList() {
+    const [info, setInfo] = useState()
+    const handleInfo = (data) => {
+        console.log(data.name);
+        setInfo(<div >
+            <h3>Họ và tên: {data.name}</h3>
+            <p>Ngày sinh: {dateFormat(data.doB, "dd/mm/yyyy")}</p>
+            <p>Ngày vào công ty: {dateFormat(data.startDate, "dd/mm/yyyy")}</p>
+            <p>Phòng ban: {data.department.name}</p>
+            <p>Số ngày nghỉ còn lại: {data.annualLeave}</p>
+            <p>Số ngày đã làm thêm: {data.overTime}</p>
+        </div>)
+    }
+
+    return (<div className='container'>
+        <div className='row'>
+            {STAFFS.map(staff => (
+                <RenderStaff
+                    key={staff.id}
+                    data={staff}
+                    onClick={handleInfo}
+                />
+            ))}
+        </div>
+        <div className='row'>
+            <div className='info-box'>{info || 'Bấm vào tên để xem thông tin'}</div>
+        </div>
+    </div>
+    )
+}
+export default StaffList;
