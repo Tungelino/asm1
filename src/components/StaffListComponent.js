03764effb4f2f112b73804228ca806d745a17608
TungNguyen <tungnguyen.neu@gmail.com>
2021-10-25 19:46:41 +0900

    commit7:mentor check

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -3,10 +3,12 @@ import { DEPARTMENTS, ROLE, STAFFS } from '../shared/staffs'
 import dateFormat from "dateformat"
 import { useState } from "react";
 import './staff.css'
+// const colums = ['col-12 col-md-4', 'col-3']
+
 
 function RenderStaff({ data, onClick }) {
     return (
-        <div className='col-lg-4 col-md-6  '>
+        <div className={colum}>
             <div className='staff-name'>
                 <p onClick={() => onClick(data)}>{data.name}</p>
             </div>
@@ -15,6 +17,12 @@ function RenderStaff({ data, onClick }) {
 }
 function StaffList() {
     const [info, setInfo] = useState()
+    const [colum, setColum] = useState('col-12 col-md-4')
+
+    const handleChange = () => {
+        setColum('col-md-3')
+    }
+
     const handleInfo = (data) => {
         setInfo(<div >
             <h3>Họ và tên: {data.name}</h3>
@@ -26,36 +34,27 @@ function StaffList() {
             <p>Số ngày đã làm thêm: {data.overTime}</p>
         </div>)
     }
-    const [changeCol, setChangeCol] = useState()
-    changeCol =
-        { className='col-lg-4 col-md-6' }
 
-
-    const handleChange = () => {
-        setChangeCol(
-            { className='col-lg-3' }
-        )
-    }
-    return (<div className='container'>
-        <div className='row'>
-            <div className='btn-change'>
-                <button onClick={handleChange}>Đổi sang giao diện 4 cột</button>
-
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='btn-change'>
+                    <button onClick={handleChange}>Đổi sang giao diện 4 cột</button>
+                </div>
+            </div>
+            <div className='row'>
+                {STAFFS.map(staff => (
+                    <RenderStaff
+                        key={staff.id}
+                        data={staff}
+                        onClick={handleInfo}
+                    />
+                ))}
+            </div>
+            <div className='row'>
+                <div className='col-12 col-md-4' >{info || 'Bấm vào tên để xem thông tin'}</div>
             </div>
         </div>
-        <div className='row'>
-            {STAFFS.map(staff => (
-                <RenderStaff
-                    key={staff.id}
-                    data={staff}
-                    onClick={handleInfo}
-                />
-            ))}
-        </div>
-        <div className='row'>
-            <div className='col-lg-4 col-md-6 info-box' >{info || 'Bấm vào tên để xem thông tin'}</div>
-        </div>
-    </div>
     )
 }
 export default StaffList;
