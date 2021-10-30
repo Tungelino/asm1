51c005134c5f539581f7525c46c92919f8cc3d5d
TungNguyen <tungnguyen.neu@gmail.com>
2021-10-31 00:07:47 +0900

    commit3: restart may

--- src/components/PayComponent.js
+++ src/components/PayComponent.js
@@ -1,6 +1,33 @@
+import React from "react";
+import { Card } from "reactstrap";
+const basicSalary = 3000000
+const overTimeSalary = 200000
+const RenderPay = ({ staff }) => {
+    return (
+        <div className='col-12 col-md-6 col-lg-4'>
+            <Card>
+                <h3>{staff.name}</h3>
+            </Card>
+        </div>
+
+    )
+}
 function Pay(props) {
     return (
-        <div>xin chao luong</div>
+        <div className='container'>
+            <h3>Bảng lương chi tiết</h3>
+            <hr />
+            <div className='row'>
+                {
+                    props.staffs.map((staff) => {
+                        <RenderPay
+                            key={staff.id}
+                            staff={staff} />
+                    })
+                }
+
+            </div>
+        </div>
     )
 }
 
