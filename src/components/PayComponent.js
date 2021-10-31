95de9f6c51a3005abd6c36c3b9b42e3a7d161850
TungNguyen <tungnguyen.neu@gmail.com>
2021-10-31 11:52:44 +0900

    commit6: render Pay done

--- src/components/PayComponent.js
+++ src/components/PayComponent.js
@@ -1,25 +1,33 @@
 import React from "react";
 import { Card } from "reactstrap";
 import './staff.css';
-// const basicSalary = 3000000
-// const overTimeSalary = 200000
+
+
+
+
+
 const RenderPay = ({ staff }) => {
+    const basicSalary = 3000000
+    const overTimeSalary = 200000
+    const payDetail = parseInt(staff.salaryScale * basicSalary + staff.overTime * overTimeSalary)
+
+
     return (
-        <div className='col-12 col-md-6 col-lg-4 renderpay-box'>
+        <div key={staff.id} className='col-12 col-md-6 col-lg-4 renderpay-box'>
             <Card>
                 <h3>{staff.name}</h3>
                 <br />
                 <p>Mã số nhân viên:{staff.id}</p>
                 <p>Hệ số lương:{staff.salaryScale}</p>
                 <p>Số giờ làm thêm:{staff.overTime}</p>
-                <div class='pay-box'>Lương: </div>
+                <div class='pay-box'>Lương: {payDetail}</div>
             </Card>
-
         </div>
-
     )
 }
 function Pay(props) {
+
+
     return (
         <div className='container'>
             <h3>Bảng lương</h3>
@@ -27,7 +35,6 @@ function Pay(props) {
             <div className='row'>
                 {props.staffs.map(staff => (
                     <RenderPay
-                        key={staff.id}
                         staff={staff}
                     />
                 ))}
