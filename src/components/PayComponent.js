32259eb19435db07ad7798c9e86100e1597abe30
TungNguyen <tungnguyen.neu@gmail.com>
2021-10-31 10:47:40 +0900

    commit5: them css

--- src/components/PayComponent.js
+++ src/components/PayComponent.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Card } from "reactstrap";
+import './staff.css';
 // const basicSalary = 3000000
 // const overTimeSalary = 200000
 const RenderPay = ({ staff }) => {
     return (
-        <div className='col-12 col-md-6 col-lg-4'>
+        <div className='col-12 col-md-6 col-lg-4 renderpay-box'>
             <Card>
                 <h3>{staff.name}</h3>
+                <br />
+                <p>Mã số nhân viên:{staff.id}</p>
+                <p>Hệ số lương:{staff.salaryScale}</p>
+                <p>Số giờ làm thêm:{staff.overTime}</p>
+                <div class='pay-box'>Lương: </div>
             </Card>
 
         </div>
@@ -16,7 +22,7 @@ const RenderPay = ({ staff }) => {
 function Pay(props) {
     return (
         <div className='container'>
-            <h3>Bảng lương chi tiết</h3>
+            <h3>Bảng lương</h3>
             <hr />
             <div className='row'>
                 {props.staffs.map(staff => (
